fe644238189e26702b426d857457916e07d92599
Shane Kelly <shaneqkelly@gmail.com>
2018-05-07 01:31:55 -0400

    Add button for toggling extra accessibility notices

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,3 +1,9 @@
+$(document).on("click", ".toggleExtraNoticesBtn", function(e) {
+  const id = e.target.id
+  $(`.extra-${id}`).toggleClass('hidden');
+});
+
+
 /*
  * Selects all of the text of all of the children within the element at the specified id, copies
  *   that text to the clipboard, and then unselects all the text
